b63cc7d10f23699bf86d23893a4c358f0491d009
Sam Buerck <samuel.buerck@jacks.sdstate.edu>
2025-02-23 14:56:04 -0600

    Settings.

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,12 +1,13 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatBottomSheet, MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { SettingsComponent } from '../settings/settings.component';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatBottomSheetModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -14,7 +15,13 @@ export class HeaderComponent {
   @Output()
   toggleSidenav = new EventEmitter<any>();
 
-  public onClick(event) {
+  private _bottomSheet = inject(MatBottomSheet);
+
+  public openSidenav(event) {
     this.toggleSidenav.emit(event);
+  }
+
+  public openSettings() {
+    this._bottomSheet.open(SettingsComponent)
   }
 }
---
+++ client/src/app/settings/settings.component.ts
@@ -0,0 +1,11 @@
+import { Component, inject } from '@angular/core';
+import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
+
+@Component({
+  selector: 'app-settings',
+  imports: [],
+  templateUrl: './settings.component.html',
+  styleUrl: './settings.component.scss'
+})
+export class SettingsComponent {
+}
