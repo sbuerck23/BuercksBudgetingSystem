1b43c954dd1745932f8a3654733688888272f7ae
Sam Buerck <samuel.buerck@jacks.sdstate.edu>
2025-02-22 23:43:34 -0600

    More app.

--- client/src/app/expense-dialog/expense-dialog.component.ts
+++ client/src/app/expense-dialog/expense-dialog.component.ts
@@ -7,10 +7,12 @@ import {
 } from '@angular/material/dialog';
 import { DialogData } from '../expense-list/expense-list.component';
 import { Expense } from '../expense';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-expense-dialog',
-  imports: [MatCardModule, MatDialogModule],
+  imports: [MatCardModule, MatDialogModule, MatIconModule, MatButtonModule],
   templateUrl: './expense-dialog.component.html',
   styleUrl: './expense-dialog.component.scss'
 })
@@ -18,4 +20,8 @@ export class ExpenseDialogComponent {
   readonly dialogRef = inject(MatDialogRef<ExpenseDialogComponent>);
   readonly data = inject<DialogData>(MAT_DIALOG_DATA);
   readonly expense: Expense = this.data
+
+  public closeDialog() {
+    this.dialogRef.close();
+  }
 }
